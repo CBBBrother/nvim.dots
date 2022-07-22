df50a5db4189efaa94ac4b76e26506b90a5e6b12
CBBBrother <CBBBrother@gmail.com>
2022-07-23 00:25:56 +0300

    fix error

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -116,8 +116,10 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  window = {
+     documentation = {
+        "╭", "─", "╮", "│", "╯", "─", "╰", "│" 
+     },
   },
   experimental = {
     ghost_text = false,
